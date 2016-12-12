83e44b4c27b6ee6470de231ac1738517be84c0c2
Peter Slagter <peter.slagter@procurios.nl>
2016-12-12 09:31:54 +0100

    Add sane defaults and Darcula theme for Sublime Text

--- sublime.sh
+++ sublime.sh
@@ -8,4 +8,8 @@ if echo "$answer" | grep -iq "^n" ;then
     exit
 fi
 
-cp -r init/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
+echo "Copying preferences..."
+cp -r sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
+echo "Copying Darcula theme..."
+cp -r sublime/Darcula.tmTheme ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Darcula.tmTheme 2> /dev/null
+echo "Done!"
